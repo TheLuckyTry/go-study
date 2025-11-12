35b4db5b6da4b175bf870b7fb5cee21986ceb1d2
陈益龙 <yilong.chen@haitrade.hk>
2025-11-13 01:21:57 +0800

    练习1

---
+++ task1/is-palind-rome/isPalindrome.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+/*
+给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+
+回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+
+例如，121 是回文，而 123 不是。
+
+示例 1：
+
+输入：x = 121
+输出：true
+示例 2：
+
+输入：x = -121
+输出：false
+解释：从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+示例 3：
+
+输入：x = 10
+输出：false
+解释：从右向左读, 为 01 。因此它不是一个回文数。
+*/
+func isPalindrome(x int) bool {
+
+	original := x
+	reversed := 0
+	for x > 0 {
+		reversed = reversed*10 + x%10
+		x /= 10
+	}
+
+	return original == reversed
+}
+func main() {
+	fmt.Println("测试结果1", isPalindrome(121))
+	fmt.Println("测试结果2", isPalindrome(-121))
+	fmt.Println("测试结果3", isPalindrome(10))
+}
